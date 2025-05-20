02a52fdbe78f4c667e8c9d48e458352e08cba21e
Christian Marangi <ansuelsmth@gmail.com>
2025-05-21 01:10:57 +0200

    airoha: update ATF/U-Boot with new eMMC support and Emergency Recovery
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

--- target/linux/airoha/image/an7581.mk
+++ target/linux/airoha/image/an7581.mk
@@ -4,6 +4,14 @@ define Build/an7581-bl2-bl31-uboot
   dd if=$(STAGING_DIR_IMAGE)/an7581_$1-bl31-uboot.img of=$@ bs=1 seek=$$((0x20000)) conv=notrunc
 endef
 
+define Build/an7581-preloader
+  cat $(STAGING_DIR_IMAGE)/an7581_$1-bl2.fip >> $@
+endef
+
+define Build/an7581-bl31-uboot
+  cat $(STAGING_DIR_IMAGE)/an7581_$1-bl31-u-boot.fip >> $@
+endef
+
 define Device/FitImageLzma
 	KERNEL_SUFFIX := -uImage.itb
 	KERNEL = kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(DEVICE_DTS).dtb
@@ -14,16 +22,33 @@ define Device/airoha_an7581-evb
   $(call Device/FitImageLzma)
   DEVICE_VENDOR := Airoha
   DEVICE_MODEL := AN7581 Evaluation Board (SNAND)
-  DEVICE_PACKAGES := kmod-leds-pwm kmod-i2c-an7581 kmod-pwm-airoha kmod-input-gpio-keys-polled
+  DEVICE_PACKAGES := kmod-leds-pwm kmod-i2c-an7581 kmod-pwm-airoha kmod-input-gpio-keys-polled \
+    kmod-usb-ledtrig-usbport
   DEVICE_DTS := an7581-evb
   DEVICE_DTS_DIR := ../dts
   DEVICE_DTS_CONFIG := config@1
   KERNEL_LOADADDR := 0x80088000
   IMAGE/sysupgrade.bin := append-kernel | pad-to 128k | append-rootfs | pad-rootfs | append-metadata
   ARTIFACT/bl2-bl31-uboot.bin := an7581-bl2-bl31-uboot rfb
-  ARTIFACTS := bl2-bl31-uboot.bin
+  ARTIFACT/preloader.bin := an7581-preloader rfb
+  ARTIFACT/bl31-uboot.fip := an7581-bl31-uboot rfb
+  ARTIFACTS := bl2-bl31-uboot.bin preloader.bin bl31-uboot.fip
 endef
 TARGET_DEVICES += airoha_an7581-evb
+
+define Device/airoha_an7581-evb-an8811
+  $(call Device/airoha_an7581-evb)
+  DEVICE_MODEL := AN7581 Evaluation Board (SNAND + AN8811)
+  DEVICE_DTS := an7581-evb-an8811
+endef
+TARGET_DEVICES += airoha_an7581-evb-an8811
+
+define Device/airoha_an7581-evb-pon
+  $(call Device/airoha_an7581-evb)
+  DEVICE_MODEL := AN7581 Evaluation Board (SNAND + PON)
+  DEVICE_DTS := an7581-evb-pon
+endef
+TARGET_DEVICES += airoha_an7581-evb-pon
 
 define Device/airoha_an7581-evb-emmc
   DEVICE_VENDOR := Airoha
@@ -31,7 +56,27 @@ define Device/airoha_an7581-evb-emmc
   DEVICE_DTS := an7581-evb-emmc
   DEVICE_DTS_DIR := ../dts
   DEVICE_PACKAGES := kmod-i2c-an7581
-  ARTIFACT/bl2-bl31-uboot.bin := an7581-bl2-bl31-uboot rfb
-  ARTIFACTS := bl2-bl31-uboot.bin
+  ARTIFACT/preloader.bin := an7581-preloader rfb
+  ARTIFACT/bl31-uboot.fip := an7581-bl31-uboot rfb
+  ARTIFACTS := preloader.bin bl31-uboot.fip
 endef
 TARGET_DEVICES += airoha_an7581-evb-emmc
+
+define Device/airoha_an7581-evb-emmc-an8831
+  DEVICE_VENDOR := Airoha
+  DEVICE_MODEL := AN7581 Evaluation Board (EMMC + AN8831)
+  DEVICE_DTS := an7581-evb-emmc-an8831
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-i2c-an7581
+  ARTIFACT/preloader.bin := an7581-preloader rfb
+  ARTIFACT/bl31-uboot.fip := an7581-bl31-uboot rfb
+  ARTIFACTS := preloader.bin bl31-uboot.fip
+endef
+TARGET_DEVICES += airoha_an7581-evb-emmc-an8831
+
+define Device/airoha_an7581-evb-10g-lan
+  $(call Device/airoha_an7581-evb)
+  DEVICE_MODEL := AN7581 Evaluation Board (SNAND + ETH-SERDES-LAN + PON)
+  DEVICE_DTS := an7581-evb-10g-lan
+endef
+TARGET_DEVICES += airoha_an7581-evb-10g-lan
